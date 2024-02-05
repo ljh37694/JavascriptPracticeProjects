cba2cbbf89f50ddf25bfdea22bc74a9f1142b2a0
ljh37694 <ljh37694@naver.com>
2024-02-05 23:35:26 +0900

    삭제 버튼 및 엔터 눌러도 리스트 추가 기능 구현

--- TodoList/main.js
+++ TodoList/main.js
@@ -1,15 +1,5 @@
 let todoLists = [];
 
-$("#add-btn").click(() => {
-    let input = $("#todo-input");
-
-
-    if (input.val()) {
-        console.log(input.val());
-        $(".todo-lists-container").append(makeTodoList(input.val()));
-    }
-});
-
 function makeTodoList(text) {
     let list = `                
         <div class="todo-list">
@@ -21,10 +11,34 @@ function makeTodoList(text) {
                     <p class="text-start">${text}</p>
                 </div>
                 <div class="col-2">
-                    <button class="btn">x</button>
+                    <button class="del-btn">x</button>
                 </div>
             </div>
         </div>`;
 
     return list;
 }
+
+function pushTodoList() {
+    let input = $("#todo-input");
+
+    if (input.val()) {
+        $(".todo-lists-container").append(makeTodoList(input.val()));
+        todoLists.push(input.val());
+        input.val("");
+    }
+
+    $(".del-btn").click(function() {
+        this.parentElement.parentElement.parentElement.remove();
+    });
+}
+
+$("#add-btn").click(function() {
+    pushTodoList();
+});
+
+$("#todo-input").on("keypress", function(e) {
+    if (e.keyCode == 13) {
+        pushTodoList();
+    }
+})
