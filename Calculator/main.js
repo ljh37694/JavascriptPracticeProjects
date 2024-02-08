fa9243d0baeeca0e74f82c296bba8a8478cd5986
ljh37694 <ljh37694@naver.com>
2024-02-08 13:35:26 +0900

    애플 계산기 조금만 함

---
+++ Calculator/main.js
@@ -0,0 +1,13 @@
+let calcBtnTexts = [
+    ["AC", "f", "%", "÷"],
+    ["7", "8", "9", "X"],
+    ["4", "5", "6", "+"],
+    ["1", "2", "3", "-"],
+    ["0", ".", "="]
+];
+
+let calcBtnContainer = $(".calc-btn-container");
+
+calcBtnTexts.forEach((arr, idx) => {
+
+});
