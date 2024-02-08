fe8f31302616860b00ca27b491fc09e17c038ccc
ljh37694 <ljh37694@naver.com>
2024-02-08 22:16:55 +0900

    계산기 버튼 나타나게 구현

--- Calculator/main.js
+++ Calculator/main.js
@@ -1,6 +1,6 @@
 let calcBtnTexts = [
     ["AC", "f", "%", "÷"],
-    ["7", "8", "9", "X"],
+    ["7", "8", "9", "x"],
     ["4", "5", "6", "+"],
     ["1", "2", "3", "-"],
     ["0", ".", "="]
@@ -8,6 +8,22 @@ let calcBtnTexts = [
 
 let calcBtnContainer = $(".calc-btn-container");
 
-calcBtnTexts.forEach((arr, idx) => {
+calcBtnTexts.forEach((arr) => {
+    let btnRow = document.createElement("div");
+    btnRow.className = "row";
 
+    arr.forEach((item) => {
+        let col = document.createElement("div");
+        let btn = document.createElement("button");
+
+        btn.className = "calc-btn";
+        col.className = (item == "0" ? "col-6" : "col-3");
+
+        btn.innerHTML = item;
+
+        col.appendChild(btn);
+        btnRow.appendChild(col);
+    });
+
+    document.getElementsByClassName("calc-btn-container")[0].appendChild(btnRow);
 });
