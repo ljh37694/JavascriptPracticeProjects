323428ca22a49fb368792d7084b0062e9ad95208
ljh37694 <ljh37694@naver.com>
2024-02-09 15:13:01 +0900

    숫자, 기능 클릭 이벤트 구현

--- Calculator/main.js
+++ Calculator/main.js
@@ -6,24 +6,87 @@ let calcBtnTexts = [
     ["0", ".", "="]
 ];
 
+let calcBtnData = [
+    [
+        { text: "AC", size: 1, type: "func" },
+        { text: "+/-", size: 1, type: "func" },
+        { text: "bks", size: 1, type: "func" },
+        { text: "÷", size: 1, type: "operator" },
+    ],
+    [
+        { text: "7", size: 1, type: "number" },
+        { text: "8", size: 1, type: "number" },
+        { text: "9", size: 1, type: "number" },
+        { text: "x", size: 1, type: "operator" },
+    ],
+    [
+        { text: "4", size: 1, type: "number" },
+        { text: "5", size: 1, type: "number" },
+        { text: "6", size: 1, type: "number" },
+        { text: "-", size: 1, type: "operator" },
+    ],
+    [
+        { text: "1", size: 1, type: "number" },
+        { text: "2", size: 1, type: "number" },
+        { text: "3", size: 1, type: "number" },
+        { text: "+", size: 1, type: "operator" },
+    ],
+    [
+        { text: "0", size: 2, type: "number" },
+        { text: ".", size: 1, type: "point" },
+        { text: "=", size: 1, type: "equal" },
+    ],
+];
+
 let calcBtnContainer = $(".calc-btn-container");
+let result = $("#result");
 
-calcBtnTexts.forEach((arr) => {
+calcBtnData.forEach((rowData) => {
     let btnRow = document.createElement("div");
-    btnRow.className = "row";
+    btnRow.className = "row mt-3";
 
-    arr.forEach((item) => {
+    rowData.forEach((item) => {
+        let { text, size, type } = item;
         let col = document.createElement("div");
         let btn = document.createElement("button");
 
-        btn.className = "calc-btn";
-        col.className = (item == "0" ? "col-6" : "col-3");
+        btn.className = `calc-btn ${type}`;
+        col.className = `col-${size * 3}`;
 
-        btn.innerHTML = item;
+        btn.innerHTML = text;
 
         col.appendChild(btn);
         btnRow.appendChild(col);
     });
 
     document.getElementsByClassName("calc-btn-container")[0].appendChild(btnRow);
+});
+
+$(".number").click((e) => {
+    let num = e.currentTarget.textContent;
+
+    if (result.text().length < 12) {
+        result.text((result.text() == "0" ? "" : result.text()) + num);
+    }
+});
+
+$(".func").click((e) => {
+    let f = e.currentTarget.textContent;
+    let num = result.text();
+
+    if (f == "AC") {
+        result.text("0");
+    }
+
+    else if (f == "+/-") {
+        result.text((num[0] == "-" ? num.substring(1) : "-" + num));
+    }
+
+    else if (f == "bks") {
+        result.text(num.slice(0, -1));
+
+        if (result.text() == "") {
+            result.text("0");
+        }
+    }
 });
